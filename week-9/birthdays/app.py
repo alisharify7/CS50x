0fb47c7b3669a78ad679a4682c03087769012134
ali sharifi <87280202+alisharifyy@users.noreply.github.com>
2022-05-13 16:58:27 +0430

    Add files via upload

--- week-9/birthdays/app.py
+++ week-9/birthdays/app.py
@@ -1,5 +1,4 @@
 import os
-import sqlite3
 from cs50 import SQL
 from flask import Flask, flash, jsonify, redirect, render_template, request, session
 
@@ -13,9 +12,6 @@ app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 # Configure CS50 Library to use SQLite database
 db = SQL("sqlite:///birthdays.db")
-
-# conn = sqlite3.connect('birthdays.db')
-# db = conn.cursor()
 
 
 @app.after_request
@@ -33,8 +29,6 @@ def index():
         name =  request.form.get('Name')
         month =  request.form.get('Month')
         day =  request.form.get('Day')
-        month = int(month)
-        day = int(day)
 
         # TODO: Add the user's entry into the database
         db.execute('INSERT INTO birthdays (name,month,day) VALUES (?,?,?)',name,month,day)
@@ -45,14 +39,6 @@ def index():
         # TODO: Display the entries in the database on index.html
         temp = db.execute('SELECT name,day,month from birthdays')
 
-        names = []
-        for name in temp:
-            n = name['name']
-            d = name['day']
-            m = name['month']
-            names.append((
-                n , str(m) + '/' + str(d),
-            ))
-        return render_template("index.html",names=names)
+        return render_template("index.html",names=temp)
 
 
