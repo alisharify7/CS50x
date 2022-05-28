dcc0abe35e85fbb3e2e4b9d0f230b9ece0772ee1
ali sharifi <87280202+alisharifyy@users.noreply.github.com>
2022-05-28 22:55:41 +0430

    Update scrabble.c

--- week-2/scrabble/scrabble.c
+++ week-2/scrabble/scrabble.c
@@ -7,10 +7,10 @@
     // created by : alisharify
     //Do not copy code 
 
-    
+
 // Points assigned to each letter of the alphabet
 int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
-char letter[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+
 
 int compute_score(string word);
 
@@ -27,35 +27,99 @@ int main(void)
     // TODO: Print the winner
     if (score1 > score2)
     {
-        printf("Player 1 Wins!\n");
+        printf("Player 1 wins!\n");
+        return 0;
     }
-    else if (score1 < score2)
+    else if (score2 > score1)
     {
-        printf("Player 2 Wins!\n");
+        printf("Player 2 wins!\n");
+        return 0;
     }
     else
     {
         printf("Tie!\n");
+        return 0;
     }
+
 }
 
 int compute_score(string word)
 {
-    // TODO: Compute and return score for string
     int sum = 0;
-    int lenght = strlen(word);
 
-    //with this for loop calculating 
-    //points of word
-    for (int i = 0 ; i < lenght; i++)
+    // TODO: Compute and return score for string
+
+
+    for(int i = 0,len = strlen(word); i < len; i++)
     {
-        for (int s = 0 ; s < 26; s++)
+        char temp = word[i];
+        if (isalpha(temp))
         {
-            if (letter[s] == toupper(word[i]))
-            {
-                sum += POINTS[s];
-            }
+            temp = toupper(temp);
+            int loc = 'A' - temp;
+            sum += POINTS[loc];
         }
     }
     return sum;
 }
+
+
+// another way
+
+//#include <ctype.h>
+//#include <cs50.h>
+//#include <stdio.h>
+//#include <string.h>
+
+    
+// Points assigned to each letter of the alphabet
+// int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
+// char letter[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+
+// int compute_score(string word);
+
+// int main(void)
+// {
+//    // Get input words from both players
+//    string word1 = get_string("Player 1: ");
+//    string word2 = get_string("Player 2: ");
+
+    // Score both words
+//    int score1 = compute_score(word1);
+//    int score2 = compute_score(word2);
+
+    // TODO: Print the winner
+//    if (score1 > score2)
+//    {
+//        printf("Player 1 Wins!\n");
+//    }
+//    else if (score1 < score2)
+//    {
+//        printf("Player 2 Wins!\n");
+//    }
+//    else
+//    {
+//        printf("Tie!\n");
+//    }
+//}
+
+//int compute_score(string word)
+//{
+    // TODO: Compute and return score for string
+//    int sum = 0;
+//    int lenght = strlen(word);
+
+    //with this for loop calculating 
+    //points of word
+//    for (int i = 0 ; i < lenght; i++)
+//    {
+//        for (int s = 0 ; s < 26; s++)
+//        {
+//            if (letter[s] == toupper(word[i]))
+//            {
+//                sum += POINTS[s];
+//            }
+//        }
+//    }
+//    return sum;
+//}
